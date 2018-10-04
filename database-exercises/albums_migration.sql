3550bfeea28b75bccd21c419b1a0cf1e0733339e
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-10-04 13:28:10 -0500

    completion of basic statements exercise

--- database-exercises/albums_migration.sql
+++ database-exercises/albums_migration.sql
@@ -3,8 +3,8 @@ USE codeup_test_db;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
-    name  VARCHAR(50) NOT NULL,
-    release_date DATE,
+    name  VARCHAR(100),
+    release_date INT(4) UNSIGNED,
     sales FLOAT UNSIGNED,
     genre VARCHAR(50),
     PRIMARY KEY (id)
